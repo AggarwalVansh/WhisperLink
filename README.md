ca80891e5b94e16c33498d5d68ddd0195d909266
Vansh Aggarwal <160574546+AggarwalVansh@users.noreply.github.com>
2024-06-17 13:01:32 +0530

    Completely Re-Wrote the code

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple Chat App
+
+## Description
+
+#### It is simple chat app, totally made using Express.JS and Socket.io in Node.JS
+
+#### Added password feature in the login page, default password is `VANSH` ;;;
+
+## Packages
+
+#### `express.js`, `socket.io`
+
+# Installation
+
+## **Type These in Terminal**
+
+```bash
+npm i express.js socket.io --save
+```
+
+# Usage
+
+```bash
+node .
+```
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const port = `6969`;
+
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", function (socket) {
+  socket.on("newuser", function (username) {
+    socket.broadcast.emit("update", username + " joined the conversation");
+  });
+
+  socket.on("exituser", function (username) {
+    socket.broadcast.emit("update", username + " left the conversation");
+  });
+
+  socket.on("chat", function (message) {
+    socket.broadcast.emit("chat", message);
+  });
+});
+
+server.listen(port);
+console.log(`Your Chat App is working properly on http://localhost:6969`);
